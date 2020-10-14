9c892f702daab64577446fb151101f606591a201
025528078pP <peachzxzx@hotmail.com>
2020-10-14 11:11:55 +0700

    change indentation from 1 tab to 2 spaces

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,32 +11,32 @@ import Forgetpassword from "./signin/Forgetpassword";
 import Repassword from "./signin/Repassword";
 import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
 const Theme = createMuiTheme({
-	palette: {
-		primary: {
-			main: "#F55E61",
-		},
-		secondary: {
-			main: "#F8FBFE",
-		},
-		background: {
-			paper: "#F8FBFE",
-		},
-	},
+  palette: {
+    primary: {
+      main: "#F55E61",
+    },
+    secondary: {
+      main: "#F8FBFE",
+    },
+    background: {
+      paper: "#F8FBFE",
+    },
+  },
 });
 function App() {
-	return (
-		<MuiThemeProvider theme={Theme}>
-			<div style={{ textAlign: "center" }}>
-				<Route exact path="/" component={Home} />
-				<Route exact path="/signin" component={Signin} />
-				<Route exact path="/signup" component={Signup} />
-				<Route exact path="/signup/dormfinder" component={DormFinder} />
-				<Route exact path="/signup/dormowner" component={DormOwner} />
-				<Route exact path="/dorm/:id" component={Dorm} />
-				<Route exact path="/signin/forgetpassword" component={Forgetpassword} />
-				<Route exact path="/signin/repassword/:id" component={Repassword} />
-			</div>
-		</MuiThemeProvider>
-	);
+  return (
+    <MuiThemeProvider theme={Theme}>
+      <div style={{ textAlign: "center" }}>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signin" component={Signin} />
+        <Route exact path="/signup" component={Signup} />
+        <Route exact path="/signup/dormfinder" component={DormFinder} />
+        <Route exact path="/signup/dormowner" component={DormOwner} />
+        <Route exact path="/dorm/:id" component={Dorm} />
+        <Route exact path="/signin/forgetpassword" component={Forgetpassword} />
+        <Route exact path="/signin/repassword/:id" component={Repassword} />
+      </div>
+    </MuiThemeProvider>
+  );
 }
 export default App;
